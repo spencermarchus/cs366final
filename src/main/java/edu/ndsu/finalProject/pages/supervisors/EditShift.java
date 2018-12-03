4030996fccaa61656196bcfc327ccca7079324ac
unknown <mckenna.obrien@ndsu.edu>
2018-12-02 21:23:23 -0600

    non working shift editing stuff

---
+++ src/main/java/edu/ndsu/finalProject/pages/supervisors/EditShift.java
@@ -0,0 +1,134 @@
+package edu.ndsu.finalProject.pages.supervisors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.SelectModel;
+import org.apache.tapestry5.ValueEncoder;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.services.SelectModelFactory;
+import edu.ndsu.finalProject.cayenne.persistent.InstructorWorking;
+import edu.ndsu.finalProject.cayenne.persistent.Shift;
+import edu.ndsu.finalProject.services.DatabaseService;
+
+@RequiresRoles("supervisor")
+public class EditShift {
+	
+	
+	@Inject
+	private DatabaseService db;
+	
+
+	@InjectComponent
+	private Form addForm;
+	
+	@Property
+	@Persist
+	private Shift shift;
+
+	@Property
+	private SelectModel selectModelInstructor;
+	
+	@Property
+	private SelectModel selectModelLessonDate;
+	
+	@Property
+	private String selectedInstructor;
+	
+	@Property
+	@Persist(PersistenceConstants.FLASH)
+	private List<String> selectedInstructors;
+	
+	@Property
+	private String selectedLessonDate;
+	
+	
+	@Inject
+	private SelectModelFactory selectModelFactory;
+		
+	void setupRender() {
+		iw = db.getNewInstructorWorking();
+		
+		
+	}
+
+	void onValidateFromAddForm(){
+				
+		if(!addForm.getHasErrors())
+		{
+			
+			for(String s : selectedInstructors)
+			{
+				
+				InstructorWorking iw = db.getNewInstructorWorking();
+				iw.setInstructor(db.getInstructorForName(iw.getObjectContext(), s));
+				iw.setDate(db.getLessonDateByToString(iw.getObjectContext(), selectedLessonDate));
+				
+				if(!(db.shiftExists(iw)))
+					iw.getObjectContext().commitChanges();
+			}
+		}
+	}
+	
+	void onPrepare() {
+		if(selectedInstructors == null)
+			selectedInstructors = new ArrayList<String>();
+		
+        // Get all persons - ask business service to find them (from the database)
+        List<String> instructorNames = db.getAllInstructorNames();
+        List<String> ldStrings = db.getAllLessonDateStrings();
+        
+        selectModelInstructor = selectModelFactory.create(instructorNames);
+        selectModelLessonDate = selectModelFactory.create(ldStrings);
+    }
+	
+	Object onSuccess() {
+		return "supervisors/AllShifts";
+	}
+	
+	
+	 public ValueEncoder<String> getInstructorEncoder() {
+
+	        return new ValueEncoder<String>() {
+
+	            @Override
+	            public String toClient(String value) {
+	                // return the given object's ID
+	                return value;
+	            }
+
+	            @Override
+	            public String toValue(String id) { 
+	                // find the Instructor object of the given ID in the database
+	                return id;
+	            }
+	        }; 
+	    }
+	 /*
+	 public ValueEncoder<LessonDate> getLessonDateEncoder() {
+
+	        return new ValueEncoder<LessonDate>() {
+
+	            @Override
+	            public String toClient(LessonDate value) {
+	                // return the given object's ID
+	                return String.valueOf(value.getPK()); 
+	            }
+
+	            @Override
+	            public LessonDate toValue(String id) { 
+	                // find the Instructor object of the given ID in the database
+	                return db.getLessonDateByPK(null, Integer.parseInt(id)); 
+	            }
+	        }; 
+	    }
+	*/
+	
+}
